3ac76eb8e8d4a2d308323c84f927e1a737ddeed7
Isabelle <isa_landini@hotmail.com>
2026-05-09 14:51:30 -0300

    feat: criando crud.py

---
+++ app/crud.py
@@ -0,0 +1,15 @@
+from app.models import RegistroFoco
+
+def create_registro_foco(db, registro):
+    novo_registro = RegistroFoco(
+        nivel_foco=registro.nivel_foco,
+        tempo_minutos=registro.tempo_minutos,
+        comentario=registro.comentario,
+        categoria=registro.categoria
+    )
+
+    db.add(novo_registro)
+    db.commit()
+    db.refresh(novo_registro)
+
+    return novo_registro
